14dc1b072cddec7048b1d5828101c993513f2b90
gureva.anj@gmail.com <gureva.anj@gmail.com>
2022-02-28 10:28:02 +0300

    Добавьте файлы проекта.

---
+++ factorial/factorial.cpp
@@ -0,0 +1,48 @@
+﻿// factorial.cpp
+#include <iostream>
+using namespace std;
+
+
+int factorial(int n)
+{
+    if (n == 0) return 1;
+    if (n < 0) return 0;               
+    else
+    {      
+        return n * factorial(n - 1);
+    }
+}
+double power(double a, int n)
+{
+    if (n < 0)return 0;
+    if (n == 0)return 1;
+    else
+    {
+        return a * power(a, n - 1);
+    }
+   
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int n; 
+    double a;
+
+    cout << "Введите число факториала: " ;
+    cin >> n;
+    cout << endl;  
+    cout<<"Результат:" << n << "! = " << factorial(n) << endl;
+    cout << endl;
+
+    cout << "Введите число a: ";
+    cin >> a;
+    cout << "Введите степень n: ";
+    cin >> n;
+    cout << endl;
+    std::cout << "Результат: " <<power(a, n) << '\n';
+    return 0;
+
+}
+
